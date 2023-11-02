cc52a1229d443a20e4148f6a574397deb0b3baf5
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-11-03 01:28:29 +0500

    Add files via upload

---
+++ Новая папка/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(ImaxMinSpeed.minSpeed);
+Console.WriteLine(ImaxMinSpeed.MaxSpeed);
+double time = ImaxMinSpeed.GetTime(500, 120);//время
+Console.WriteLine(time);
+
+ 
+interface ImaxMinSpeed
+{
+    public const int minSpeed = 0; //минимальное скорость
+    private static int maxSpeed = 120; //максимальная скорость
+    static double GetTime(double distance, double speed) => distance / speed;
+    static int MaxSpeed
+    {
+        get => maxSpeed;
+        set
+        {
+            if (value > 120) maxSpeed = value;
+        }
+    }
+}
